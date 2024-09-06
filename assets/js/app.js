4b7db7d4a6bcbdb1dca6fff6ec09fdcce7dc9cc7
Arif Darmawan <63298577+Arrifdarmawan@users.noreply.github.com>
2024-09-07 03:05:42 +0700

    Update app.js
    Hapus watermark

--- assets/js/app.js
+++ assets/js/app.js
@@ -448,22 +448,3 @@ L.control.betterscale({
 	metric: true,
 	imperial: false
 }).addTo(map);
-
-/* Logo watermark */
-L.Control.Watermark = L.Control.extend({
-	onAdd: function(map) {
-		var img = L.DomUtil.create('img');
-		img.src = 'assets/img/UMS.png';
-		img.style.width = '50px';
-			return img;
-	},
-	onRemove: function(map) {
-		// Nothing to do here
-	}
-});
-
-L.control.watermark = function(opts) {
-	return new L.Control.Watermark(opts);
-}
-
-L.control.watermark({ position: 'bottomleft' }).addTo(map);
